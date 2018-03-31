4cfb855322b1b5e28392355de4721b3698230712
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-31 16:08:26 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
-#def prime?(number)
-  #if number % 2 != 0 && number 
-    #true
-  #else number <= 1 &&
-    #false
-  #end
-#end
-
 def prime?(number)
-  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
-  return true
+  if number % 2 != 0 && number 
+    true
+  else number <= 1 &&
+    false
+  end
 end
+
+#def prime?(number)
+  #(2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+  #return true
+#end
