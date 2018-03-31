fb7d0f189ce25d4f9389861d4ec646929fd909dc
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-31 16:20:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   if (2..number-1).each {|n| number % 2 != 0 && number % n != 0}
   #if number % 2 != 0 && number 
     true
-  else number <= 1 && 
+  else number <= 1 && number % n == 0
     false
   end
 end
