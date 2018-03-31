ffa18b5b988459c5989fcbc8425d41cb92238a7f
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-03-31 17:01:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 
 def prime?(number)
-  if number <= 2 && number % n == 0
+  if number < 2 && number % n == 0
     false
   else (2..number-1).all? {|n| number % 2 != 0 && number % n != 0}
     true
